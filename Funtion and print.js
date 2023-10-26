f878d68d72b6f8a33a003796068c3ec8cec7ef7b
Mahedi-hasan1 <mmhasan5365@gmail.com>
2023-10-26 19:28:41 +0600

    funtion and print in javascript

---
+++ Funtion and print.js
@@ -0,0 +1,23 @@
+/// variable declaring
+let a ; // can change later
+const b = 5; // can't change later
+
+// function defination
+function get(x,y){
+
+}
+// function expression with anymCorous function
+let fnVar = function(){
+
+}
+// function expression with named function
+let fnNamedVar = function name(){
+
+}
+// arrow function
+let arrow = (a, b) => {
+    
+}
+let multByTen = (a) => (a*10);
+res =   multByTen (a);
+console.log(result);
